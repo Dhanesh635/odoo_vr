d717b0ccd1d30ad9d49611e164e7d2c5b1b53ea1
Pushpraj-10 <pushpraj.nareti@gmail.com>
2026-07-12 16:55:04 +0530

    Fix: CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', // Next.js
+      'http://localhost:3001', // If using another port
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
